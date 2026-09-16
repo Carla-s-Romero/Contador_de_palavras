0a9be1c785cccfcc6699b5dfa429f5e7556bc5d4
Carla-s-Romero <carlasromero11@gmail.com>
2026-09-15 21:59:50 -0300

    adiciona arquivos de configuração e implementa CLI para processamento de texto

--- src/cli.js
+++ src/cli.js
@@ -2,23 +2,53 @@ import fs from "fs";
 import errorHandling from "./errorHandling.js";
 import { contador } from "./index.js";
 import { montaSaida } from "./helper.js";
+import { Command } from "commander";
+import path from "path";
 
-const caminho = process.argv;
-const link = caminho[2];
-const endereco = caminho[3];
+const program = new Command();
 
-fs.readFile(link, "utf-8", async (erro, text) => {
-  try {
-    if (erro) throw erro;
+program
+  .version("0.0.1")
+  .option("-t, --texto <string>", "Caminho do texto a ser processado")
+  .option(
+    "-d, --destino <string>",
+    " Caminho da pasta onde salvar o arquivo resultado",
+  )
+  .action((options) => {
+    const { texto, destino } = options;
 
-    const resultado = contador(text);
+    if (!texto || !destino) {
+      console.error("erro: por favor inserir o caminho de origem e destino");
+      program.help();
+      return;
+    }
 
-    await criarSalvarArquivos(resultado, endereco);
+    const caminhoTexto = path.resolve(texto);
+    const caminhoDestino = path.resolve(destino);
 
-  } catch (erro) {
-    errorHandling(erro);
-  }
-});
+    try {
+      processarArquivo(caminhoTexto, caminhoDestino);
+      console.log("texto processado com sucesso");
+    } catch (error) {
+      console.log("Ocorreu um erro no processamento", error);
+    }
+  });
+
+program.parse();
+
+function processarArquivo(texto, destino) {
+  fs.readFile(texto, "utf-8", async (erro, text) => {
+    try {
+      if (erro) throw erro;
+
+      const resultado = contador(text);
+
+      await criarSalvarArquivos(resultado, destino);
+    } catch (erro) {
+      errorHandling(erro);
+    }
+  });
+}
 
 async function criarSalvarArquivos(listaPalavras, endereco) {
   const novoArquivo = `${endereco}/resultado.txt`;
